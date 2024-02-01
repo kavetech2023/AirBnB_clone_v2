2095ee5b4a7f5af9e2642ddd08559e1ae63e5fce
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-02-01 22:06:28 +0300

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,27 +1,82 @@
 #!/usr/bin/python3
-"""
-Deletes out-of-date archives
-fab -f 100-clean_web_static.py do_clean:number=2
-    -i ssh-key -u ubuntu > /dev/null 2>&1
-"""
+"""Defines fabric functions that distributes
+an archive to web servers, deploys it, and cleans up after"""
 
-import os
 from fabric.api import *
+from datetime import datetime
+import os
 
-env.hosts = ['100.25.3.52', '100.25.147.79']
+env.hosts = [
+    "100.25.3.52",
+    "100.25.147.79"
+]
+
+
+def do_pack():
+    """Generate an archive of /web_static folder"""
+    try:
+        os.makedirs("versions", exist_ok=True)
+        date = datetime.now().strftime('%Y%m%d%H%M%S')
+        fn = "versions/web_static_{}.tgz".format(date)
+
+        out = local("tar -czvf {} ./web_static/".format(fn))
+
+        if out.succeeded:
+            return "./{}".format(out.command.split(" ")[2])
+        else:
+            return None
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """distributes an archive to web servers
+    """
+    archive_exists = os.path.exists(archive_path)
+    if not archive_exists:
+        return False
+
+    try:
+        archive_fn = archive_path.split("/")[-1]
+        archive_dir = "/data/web_static/releases/{}".format(
+            archive_fn.split(".")[0])
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(archive_dir))
+        run("tar -xzf /tmp/{fn} --directory {dir}".format(
+            fn=archive_fn, dir=archive_dir))
+        run("rsync -a {dir}/web_static/* {dir}".format(dir=archive_dir))
+        sudo("chown -R ubuntu:ubuntu /data/")
+        run("rm -rf {dir}/web_static".format(dir=archive_dir))
+        run("rm /tmp/{}".format(archive_fn))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf {} /data/web_static/current".format(archive_dir))
+        return True
+    except Exception:
+        return False
 
 
 def do_clean(number=0):
-
-    number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
+    """Delete (total - number) amount of out-of-date archives
+    """
+    number = int(number)
+    with lcd("./versions"):
+        if number > 1:
+            local("ls -t | tail -n +{} | xargs rm -f".format(number + 1))
+        else:
+            local("ls -t | tail -n +2 | xargs rm -f")
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
+        if number > 1:
+            run("ls -t | tail -n +{} | xargs rm -rf".format(number + 1))
+        else:
+            run("ls -t | tail -n +2 | xargs rm -rf")
+
+
+def deploy():
+    """Archives and deploys the web_static folder"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+
+    return do_deploy(archive_path)
         [run("rm -rf ./{}".format(a)) for a in archives]
