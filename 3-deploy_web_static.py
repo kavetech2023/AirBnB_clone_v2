c5397c96ae90571c016f124dd59c2caaea4c7b93
root <kavetech2023@gmail.com>
2024-02-02 23:48:38 -0800

    update 3-deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,59 +1,62 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
-
-execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
-"""
-
-from fabric.api import env, local, put, run
+from fabric.api import *
 from datetime import datetime
-from os.path import exists, isdir
-env.hosts = ['100.25.3.52', '100.25.147.79']
-=======
-env.hosts = [ 
-    "100.25.3.52",
-    "100.25.147.79"
-]
+import os
+from os.path import exists
+
+
+"""Fabric script that generates a .tgz archive from
+the contents of the web_static folder"""
 
 
 def do_pack():
-    """generates a tgz archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
+    """Create a tar gzipped archive of the web_static folder"""
+    # Create the versions folder if it doesn't exist
+    if not os.path.exists("versions"):
+        local("mkdir -p versions")
+    # Generate the archive name based on the current date and time
+    now = datetime.now()
+    archive_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year,
+                                                        now.month,
+                                                        now.day,
+                                                        now.hour,
+                                                        now.minute,
+                                                        now.second)
+    # Create the archive using the tar command
+    archive_path = "versions/{}".format(archive_name)
+    result = local("tar -cvzf {} web_static".format(archive_path))
+    # Return the archive path if successful, otherwise None
+    if result.succeeded:
+        return archive_path
+    else:
         return None
 
 
+"""a Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers"""
+
+
+env.hosts = ['100.25.3.52', '35.153.226.125']
+
+
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive to the web servers"""
+    # Check if the archive path exists
+    if not exists(archive_path):
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-        return False
-
-
-def deploy():
-    """creates and distributes an archive to the web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    return do_deploy(archive_path)
+    # Get the archive filename without extension
+    archive_file = archive_path.split('/')[-1]
+    archive_name = archive_file.split('.')[0]
+    # Define the remote paths
+    tmp_path = "/tmp/{}".format(archive_file)
+    release_path = "/data/web_static/releases/{}".format(archive_name)
+    current_path = "/data/web_static/current"
+    # Upload the archive to the /tmp/ directory of the web server
+    put(archive_path, tmp_path)
+    # Uncompress the archive to the /data/web_static/releases/
+    run("mkdir -p {}".format(release_path))
+    run("tar -xzf {} -C {}".format(tmp_path, release_path))
+    # Delete the archive from the web server
+    run("rm {}".format(tmp_path))
+    # Delete the symbolic link /data/web_static/current from the web server
+    run("mv {}/web_static/* {}".format(release_p
